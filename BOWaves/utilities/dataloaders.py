c7db1899b6f94e256d12c26da1617a6a369d4468
Austin <austin.meek10@gmail.com>
2023-08-17 11:56:54 -0400

    load and visualize Frolich's EEG data from .mat files

--- BOWaves/utilities/dataloaders.py
+++ BOWaves/utilities/dataloaders.py
@@ -19,6 +19,9 @@ Where the functions housed in the current file come into play in the above pipel
 
 from scipy.io import loadmat
 from pathlib import Path
+import numpy as np
+import matplotlib.pyplot as plt
+import os
 
 def load_ics_from_matlab(root: Path, dataset_name: str):
     """
@@ -37,3 +40,58 @@ def load_ics_from_matlab(root: Path, dataset_name: str):
     """
 
     data_dir = root.joinpath('data', dataset_name)
+
+
+def load_and_visualize_mat_file_frolich(file_path):
+    # Create 'img' subdirectory if it doesn't exist
+    img_dir = os.path.join(os.path.dirname(file_path), 'img')
+    os.makedirs(img_dir, exist_ok=True)
+
+    # Load .mat file
+    data = loadmat(file_path)
+
+    # Display metadata
+    print("Metadata:")
+    for key, value in data.items():
+        if not key.startswith("__"):
+            print(f"{key}: {type(value)}")
+            # if isinstance(value, np.ndarray):
+            #     print(value)
+
+    # Visualize EEG time series data
+    for key, value in data.items():
+        if isinstance(value, np.ndarray) and value.dtype in (np.float32, np.float64, np.int32, np.int64):
+            if value.ndim == 2:
+                num_channels, num_samples = value.shape
+                time = np.arange(num_samples)  # Assuming time starts from 0 and is evenly spaced
+
+                fig, axes = plt.subplots(num_channels, 1, figsize=(10, 3 * num_channels))
+                fig.suptitle(key)
+
+                for channel in range(num_channels):
+                    axes[channel].plot(time, value[channel])
+                    #axes[channel].set_ylabel(f'Channel {channel + 1}')
+                    if not (key == "X" and channel == 63):
+                        print(channel)
+                        axes[channel].set_ylabel(f'Channel {channel} \n Label {data["classes"][0][data["labels"][channel] - 1][0][0]}')
+
+                axes[-1].set_xlabel('Time')
+                plt.subplots_adjust(hspace=0.5)  # Adjust vertical spacing between subplots
+
+                # Save the plot to the 'img' subdirectory
+                plot_filename = os.path.join(img_dir, f"{key}_plot.png")
+                plt.savefig(plot_filename)
+                plt.close()
+
+            else:
+                print(f"Skipped visualization for {key} due to unsupported shape.")
+
+    plt.show()
+
+    #print(data['classes'][0][0])
+
+    for count, i in enumerate(data['labels']):
+        print(f"IC #{count} is label {data['classes'][0][i-1][0][0]}")
+
+# Replace 'your_file.mat' with the actual file path
+load_and_visualize_mat_file_frolich('../../data/frolich/frolich_extract_01.mat')
