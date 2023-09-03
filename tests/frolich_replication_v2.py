ee1235eca77552ab6110a87c28450e78dda12e53
Austin <austin.meek10@gmail.com>
2023-09-03 14:58:38 -0400

    hpc slurm

--- tests/frolich_replication_v2.py
+++ tests/frolich_replication_v2.py
@@ -135,18 +135,20 @@ for label in all_classes:
             time_idx = time_idx[:, None] + np.arange(window_len)[None, :]
 
             #print all relevant info up to this point for debugging
+            print("----------------------------------")
             print('ic: ' + str(ic))
             print('windows_per_ic: ' + str(windows_per_ic))
             print('time_idx: ' + str(time_idx))
             print('win_start: ' + str(win_start))
             print('win_start + windows_per_ic: ' + str(win_start + windows_per_ic))
-            print('ic[time_idx]: ' + str(ic[time_idx]))
-            print('X[win_start:win_start+windows_per_ic]: ' + str(X[win_start:win_start+windows_per_ic]))
+            #print('ic[time_idx]: ' + str(ic[time_idx]))
+            #print('X[win_start:win_start+windows_per_ic]: ' + str(X[win_start:win_start+windows_per_ic]))
             print('ic[time_idx].shape: ' + str(ic[time_idx].shape))
             print('X[win_start:win_start+windows_per_ic].shape: ' + str(X[win_start:win_start+windows_per_ic].shape))
             print('X.shape: ' + str(X.shape))
             print('ic.shape: ' + str(ic.shape))
 
+            #since number of windows
 
             X[win_start:win_start+windows_per_ic] = ic[time_idx]
             win_start += windows_per_ic
