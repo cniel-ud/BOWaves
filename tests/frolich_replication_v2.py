002e98e5d94fa698ad02c176fdec2b7e5f2dc2c6
Austin <austin.meek10@gmail.com>
2023-09-03 16:33:45 -0400

    hpc slurm

--- tests/frolich_replication_v2.py
+++ tests/frolich_replication_v2.py
@@ -17,6 +17,7 @@ pyrootutils.set_root(path='/work/cniel/ajmeek/BOWaves/BOWaves', pythonpath=True)
 import BOWaves.utilities.dataloaders as dataloaders
 from BOWaves.sikmeans.sikmeans_core import shift_invariant_k_means, _assignment_step
 
+print("Error check - Caviness jobs don't seem to be running")
 
 def bag_of_waves(raw_ics, codebooks):
     """
