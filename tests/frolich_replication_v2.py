1f45f139d65f7a0f3b5cf7dc5c02838fd8a7ed8e
Austin <austin.meek10@gmail.com>
2023-09-03 15:37:37 -0400

    hpc slurm

--- tests/frolich_replication_v2.py
+++ tests/frolich_replication_v2.py
@@ -160,7 +160,7 @@ for label in all_classes:
             win_start += windows_per_ic
 
     windows_per_class[label['name']] = X
-    print(label + ': ' + str(type(windows_per_class[label])))
+    print(label['name'] + ': ' + str(type(windows_per_class[label])))
 
 from BOWaves.sikmeans.sikmeans_core import shift_invariant_k_means
 metric, init = 'cosine', 'random'
