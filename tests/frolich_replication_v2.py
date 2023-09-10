b40df19f01896bcc187b3b06326c74dd6ee06dc7
Austin <austin.meek10@gmail.com>
2023-09-10 19:46:45 -0400

    hpc slurm

--- tests/frolich_replication_v2.py
+++ tests/frolich_replication_v2.py
@@ -107,12 +107,12 @@ X_train, X_test, y_train, y_test = [], [], [], []
 
 print("list: ", frolich_ics_by_subject[:10])
 selected_dataframes = [frolich_ics_by_subject[index] for index in range(10)]
-X_train.extend([df['ICs'] for df in selected_dataframes])
-y_train.extend([df['labels'] for df in selected_dataframes])
+X_train.extend(df['ICs'] for df in selected_dataframes)
+y_train.extend(df['labels'] for df in selected_dataframes)
 
 selected_dataframes = [frolich_ics_by_subject[index] for index in [10, 11]]
-X_test.extend([df['ICs'] for df in selected_dataframes])
-y_test.extend([df['labels'] for df in selected_dataframes])
+X_test.extend(df['ICs'] for df in selected_dataframes)
+y_test.extend(df['labels'] for df in selected_dataframes)
 
 if len(X_train) != len(y_train):
     raise ValueError('X_train and y_train are not the same length.')
