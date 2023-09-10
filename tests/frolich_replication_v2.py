cf39a98f9eb9069c97f2ed983ecb15b6da89cdad
Austin <austin.meek10@gmail.com>
2023-09-10 19:39:54 -0400

    hpc slurm

--- tests/frolich_replication_v2.py
+++ tests/frolich_replication_v2.py
@@ -107,7 +107,7 @@ X_train, X_test, y_train, y_test = [], [], [], []
 
 print("list: ", frolich_ics_by_subject[:10])
 selected_dataframes = [frolich_ics_by_subject[index] for index in range(10)]
-X_train.extedn([df['ICs'] for df in selected_dataframes])
+X_train.extend([df['ICs'] for df in selected_dataframes])
 y_train.extend([df['labels'] for df in selected_dataframes])
 
 selected_dataframes = [frolich_ics_by_subject[index] for index in [10, 11, 12]]
