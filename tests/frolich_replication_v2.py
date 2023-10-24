9a95ca3346c45edf9585c4c180573661b9e06274
Austin <austin.meek10@gmail.com>
2023-10-24 14:17:57 -0400

    new codebook params

--- tests/frolich_replication_v2.py
+++ tests/frolich_replication_v2.py
@@ -83,6 +83,20 @@ def train_and_store_codebooks(frolich_ICs_by_subj, loo_subj = None):
 
     """
 
+    # TODO - standardize with an object of command line args, as Carlos had it. clean this script.
+
+    # Here are the hyperparams. Remember, this is for cue dataset with sampling rate 500 Hz (see Frolich et al 2015)
+
+    # Carlos has recommended a centroid length of one second, and a window length of 1.5 times centroid length.
+
+    window_len = 750
+    metric, init = 'cosine', 'random'
+    num_clusters = 16
+    centroid_len = 500
+    n_runs = 3
+    n_jobs = 1
+    rng = 42
+
     # train_subjects = [f'{i+1:02}' for i in range(12)]
     # train_subjects.remove(loo_subj)
 
@@ -152,7 +166,7 @@ def train_and_store_codebooks(frolich_ICs_by_subj, loo_subj = None):
 
 
 
-    window_len = 256  # what's the sampling rate again?
+    # window_len = 256  # what's the sampling rate again?
 
     # where am I saving this or calling sikmeans? Think I did this wrong
     # oh. I was doing that before wrapping it in this function.
@@ -163,8 +177,7 @@ def train_and_store_codebooks(frolich_ICs_by_subj, loo_subj = None):
     for label in all_classes_train:
         # iterate through class and collect info about sequence lengths
         ic_lengths = []
-        n_ics = len(label[
-                        'ICs'])  # need to change, this thinks it's talking about a str not the dict. way to do what I want in python?
+        n_ics = len(label['ICs'])  # need to change, this thinks it's talking about a str not the dict. way to do what I want in python?
         for ic in label['ICs']:
             ic_lengths.append(len(ic))
 
@@ -209,12 +222,12 @@ def train_and_store_codebooks(frolich_ICs_by_subj, loo_subj = None):
 
     #now calculate sikmeans on the above windows per class.
 
-    metric, init = 'cosine', 'random'
-    num_clusters = 16
-    centroid_len = 256
-    n_runs = 3
-    n_jobs = 1
-    rng = 42#np.random.RandomState(42)
+    # metric, init = 'cosine', 'random'
+    # num_clusters = 16
+    # centroid_len = 256
+    # n_runs = 3
+    # n_jobs = 1
+    # rng = 42
 
     #error checking of type in windows per class
     print("after training codebooks")
@@ -245,7 +258,7 @@ def train_and_store_codebooks(frolich_ICs_by_subj, loo_subj = None):
                      shifts=label['shifts'], distances=label['distances'], inertia=label['inertia'])
 
     #now redo whole process for the lone test subject
-    window_len = 256
+    # window_len = 256
 
     windows_per_class = {'neural': [], 'blink': [], 'muscle': [], 'mixed': [], 'lateyes': [], 'heart': []}
 
@@ -298,12 +311,12 @@ def train_and_store_codebooks(frolich_ICs_by_subj, loo_subj = None):
 
     path = pyrootutils.find_root(search_from=__file__, indicator=".git")
 
-    metric, init = 'cosine', 'random'
-    num_clusters = 16
-    centroid_len = 256
-    n_runs = 3
-    n_jobs = 1
-    rng = 42#np.random.RandomState(42)
+    # metric, init = 'cosine', 'random'
+    # num_clusters = 16
+    # centroid_len = 256
+    # n_runs = 3
+    # n_jobs = 1
+    # rng = 42#np.random.RandomState(42)
 
     #error checking of type in windows per class
     print("after training codebooks")
@@ -390,139 +403,9 @@ for file in frolich_data:
 loo_subj = args.loo_subj
 train_and_store_codebooks(frolich_ics_by_subject, loo_subj=loo_subj)
 
-#train codebooks by subject and class
-# for i in range(12):
-#     train_and_store_codebooks(frolich_ics_by_subject, loo_subj=i)
-# train_and_store_codebooks(X_train, y_train, train_or_test='train')
-# train_and_store_codebooks(X_test, y_test, train_or_test='test')
 
-#
-#
-# neural = {'name': 'neural', 'ICs': [], 'centroids': [], 'labels': [], 'shifts': [], 'distances': [], 'inertia': []}
-# blink = {'name': 'blink', 'ICs': [], 'centroids': [], 'labels': [], 'shifts': [], 'distances': [], 'inertia': []}
-# muscle = {'name': 'muscle', 'ICs': [], 'centroids': [], 'labels': [], 'shifts': [], 'distances': [], 'inertia': []}
-# mixed = {'name': 'mixed', 'ICs': [], 'centroids': [], 'labels': [], 'shifts': [], 'distances': [], 'inertia': []}
-# lateyes = {'name': 'lateyes', 'ICs': [], 'centroids': [], 'labels': [], 'shifts': [], 'distances': [], 'inertia': []}
-# heart = {'name': 'heart', 'ICs': [], 'centroids': [], 'labels': [], 'shifts': [], 'distances': [], 'inertia': []}
-#
-# all_classes = [neural, blink, muscle, mixed, lateyes, heart]
-#
-# for i in range(len(X_train)):
-#     if y_train[i] == 'neural':
-#         neural['ICs'].append(X_train[i])
-#     elif y_train[i] == 'blink':
-#         blink['ICs'].append(X_train[i])
-#     elif y_train[i] == 'muscle':
-#         muscle['ICs'].append(X_train[i])
-#     elif y_train[i] == 'mixed':
-#         mixed['ICs'].append(X_train[i])
-#     elif y_train[i] == 'lateyes':
-#         lateyes['ICs'].append(X_train[i])
-#     elif y_train[i] == 'heart':
-#         heart['ICs'].append(X_train[i])
-#     else:
-#         raise ValueError('Unknown class label: ' + y_train[i])
-#
-# #grab windows from the ICs
-# #not all ICs have the same length, so must take windows. This is what we pass in to sikmeans anyways. Different windows for each class.
-#
-# #what is the window len? Set hyperparams above. Later on will modify to be chosen from grid search in nested cross validation.
-# window_len = 256 #what's the sampling rate again?
-#
-# windows_per_class = {'neural': [], 'blink': [], 'muscle': [], 'mixed': [], 'lateyes': [], 'heart': []}
-#
-# tot_num_windows = 0 #to house total number of windows for all ICs
-# for label in all_classes:
-#     #iterate through class and collect info about sequence lengths
-#     ic_lengths = []
-#     n_ics = len(label['ICs']) #need to change, this thinks it's talking about a str not the dict. way to do what I want in python?
-#     for ic in label['ICs']:
-#         ic_lengths.append(len(ic))
-#
-#     #Currently, assuming that we are not taking a subset of the ICs at all. Carlos had the option for that in his earlier window code.
-#     #So the number of windows per ic will just be the length of each ic / win len.
-#     n_windows_per_ic = [ic_len // window_len for ic_len in ic_lengths]
-#     tot_num_windows += sum(n_windows_per_ic)
-#
-#     #Now that we have the number of windows per ic, we can create the windows.
-#
-#     rng = np.random.RandomState(42)
-#
-#     X = np.zeros((tot_num_windows, window_len)) #X is for each class. Stack later
-#     win_start = 0
-#     #for label in all_classes:
-#     for ic in label['ICs']:
-#         windows_per_ic = len(ic) // window_len
-#         time_idx = np.arange(0, len(ic)-window_len+1, window_len)
-#         time_idx = rng.choice(time_idx, size=windows_per_ic, replace=False)
-#         time_idx = time_idx[:, None] + np.arange(window_len)[None, :]
-#
-#         # #print all relevant info up to this point for debugging
-#         # print("----------------------------------")
-#         # #print('ic: ' + str(ic))
-#         # print('windows_per_ic: ' + str(windows_per_ic))
-#         # #print('time_idx: ' + str(time_idx))
-#         # print('win_start: ' + str(win_start))
-#         # print('win_start + windows_per_ic: ' + str(win_start + windows_per_ic))
-#         # #print('ic[time_idx]: ' + str(ic[time_idx]))
-#         # #print('X[win_start:win_start+windows_per_ic]: ' + str(X[win_start:win_start+windows_per_ic]))
-#         # print('ic[time_idx].shape: ' + str(ic[time_idx].shape))
-#         # print('X[win_start:win_start+windows_per_ic].shape: ' + str(X[win_start:win_start+windows_per_ic].shape))
-#         # print('X.shape: ' + str(X.shape))
-#         # print('ic.shape: ' + str(ic.shape))
-#
-#         #There seems to be an off by one error here.
-#         #The very last IC goes past the total num of windows in X.
-#         #Not sure if it's off by one, since the first portion of X is filled. Perhaps it's about how I calc
-#         # the total num of ICs? - don't think so. So cut off at the last time.
-#
-#         if(win_start == tot_num_windows):
-#             break
-#
-#         X[win_start:win_start+windows_per_ic] = ic[time_idx]
-#         win_start += windows_per_ic
-#
-#     if label['name'] == 'neural':
-#         windows_per_class['neural'] = X
-#     windows_per_class[label['name']] = X
-#     print("type of first element at every iteration: \t", type(windows_per_class['neural']))
-#     print(label['name'] + ': ' + str(type(windows_per_class[label['name']])))
-#     print("type of X: ", type(X))
-#
-# from BOWaves.sikmeans.sikmeans_core import shift_invariant_k_means
-# metric, init = 'cosine', 'random'
-# num_clusters = 16
-# centroid_len = 256
-# n_runs = 3
-# n_jobs = 1
-# rng = 42#np.random.RandomState(42)
-#
-# #error checking of type in windows per class
-# print("after training codebooks")
-# for label in windows_per_class:
-#     print(label + ': ' + str(type(windows_per_class[label])))
-#
-# #need to do this per class.
-# neural['centroids'], neural['labels'], neural['shifts'], neural['distances'], neural['inertia'], _ = \
-#     shift_invariant_k_means(windows_per_class['neural'], num_clusters, centroid_len, metric=metric, init=init, n_init=n_runs, rng=rng,  verbose=True)
-# blink['centroids'], blink['labels'], blink['shifts'], blink['distances'], blink['inertia'], _ = \
-#     shift_invariant_k_means(windows_per_class['blink'], num_clusters, centroid_len, metric=metric, init=init, n_init=n_runs, rng=rng,  verbose=True)
-# muscle['centroids'], muscle['labels'], muscle['shifts'], muscle['distances'], muscle['inertia'], _ = \
-#     shift_invariant_k_means(windows_per_class['muscle'], num_clusters, centroid_len, metric=metric, init=init, n_init=n_runs, rng=rng,  verbose=True)
-# mixed['centroids'], mixed['labels'], mixed['shifts'], mixed['distances'], mixed['inertia'], _ = \
-#     shift_invariant_k_means(windows_per_class['mixed'], num_clusters, centroid_len, metric=metric, init=init, n_init=n_runs, rng=rng,  verbose=True)
-# lateyes['centroids'], lateyes['labels'], lateyes['shifts'], lateyes['distances'], lateyes['inertia'], _ = \
-#     shift_invariant_k_means(windows_per_class['lateyes'], num_clusters, centroid_len, metric=metric, init=init, n_init=n_runs, rng=rng,  verbose=True)
-# heart['centroids'], heart['labels'], heart['shifts'], heart['distances'], heart['inertia'], _ = \
-#     shift_invariant_k_means(windows_per_class['heart'], num_clusters, centroid_len, metric=metric, init=init, n_init=n_runs, rng=rng,  verbose=True)
-#
-# #sanity check
-# for label in windows_per_class:
-#     print(label + ': ' + str(windows_per_class[label].shape))
-#
-# codebooks = neural['centroids'] + blink['centroids'] + muscle['centroids'] + mixed['centroids'] + lateyes['centroids'] + heart['centroids']
-#
-# #split by subject for LOO
+# below here is some other notes / code on training clf. just use this file to do codebooks for now
+
 #
 # subjects = [i+1 for i in range(12)]
 # bowav_rep_by_sub = [] #bag of waves representation by subject. above list will constitute the labels
