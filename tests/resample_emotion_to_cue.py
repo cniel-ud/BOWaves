ba491cbc925538b63b2e3892000ad90e8c94c41a
Austin <austin.meek10@gmail.com>
2023-10-26 13:29:53 -0400

    hpc

--- tests/resample_emotion_to_cue.py
+++ tests/resample_emotion_to_cue.py
@@ -19,7 +19,7 @@ import numpy as np
 #codebook = np.load('/work/cniel/ajmeek/BOWaves/BOWaves/data/codebooks/emotion/sikmeans_P-256_k-16_class-1_minutesPerIC-50_icsPerSubj-2.npz')
 
 for i in range(7):
-    codebook = np.load(f'/work/cniel/ajmeek/BOWaves/BOWaves/data/codebooks/emotion/sikmeans_P-256_k-16_class-{i+1}_minutesPerIC-50_icsPerSubj-2.npz')
+    codebook = np.load(f'/work/cniel/ajmeek/BOWaves/BOWaves/data/codebooks/emotion/sikmeans_P-256_k-128_class-{i+1}_minutesPerIC-50_icsPerSubj-2.npz')
     centroids = codebook['centroids']
 
 
@@ -30,4 +30,4 @@ for i in range(7):
 
     # save new centroids
     # do I need to specifically save all the other things?
-    np.savez(f'/work/cniel/ajmeek/BOWaves/BOWaves/data/codebooks/emotion_resampled_to_cue/sikmeans_P-256_k-16_class-{i+1}_minutesPerIC-50_icsPerSubj-2_resampled.npz', centroids=new_centroids)
+    np.savez(f'/work/cniel/ajmeek/BOWaves/BOWaves/data/codebooks/emotion_resampled_to_cue/sikmeans_P-256_k-128_class-{i+1}_minutesPerIC-50_icsPerSubj-2_resampled.npz', centroids=new_centroids)
