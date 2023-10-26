ca0c32828258ace2ddd776c64f5a4d0f5a45f345
Austin <austin.meek10@gmail.com>
2023-10-26 13:46:13 -0400

    hpc

--- tests/resample_emotion_to_cue.py
+++ tests/resample_emotion_to_cue.py
@@ -14,12 +14,16 @@ I only care about the learned centroids for the BOW algo so that's the only thin
 
 import scipy
 import numpy as np
+import pyrootutils
+
+pyrootutils.set_root(path='/work/cniel/ajmeek/BOWaves/BOWaves', pythonpath=True)
+
 
 # load codebook
 #codebook = np.load('/work/cniel/ajmeek/BOWaves/BOWaves/data/codebooks/emotion/sikmeans_P-256_k-16_class-1_minutesPerIC-50_icsPerSubj-2.npz')
 
 for i in range(7):
-    codebook = np.load(f'/work/cniel/ajmeek/BOWaves/BOWaves/data/codebooks/emotion/sikmeans_P-256_k-128_class-{i+1}_minutesPerIC-50_icsPerSubj-2.npz')
+    codebook = np.load(f'../data/codebooks/emotion/sikmeans_P-256_k-128_class-{i+1}_minutesPerIC-50_icsPerSubj-2.npz')
     centroids = codebook['centroids']
 
 
@@ -30,4 +34,4 @@ for i in range(7):
 
     # save new centroids
     # do I need to specifically save all the other things?
-    np.savez(f'/work/cniel/ajmeek/BOWaves/BOWaves/data/codebooks/emotion_resampled_to_cue/sikmeans_P-256_k-128_class-{i+1}_minutesPerIC-50_icsPerSubj-2_resampled.npz', centroids=new_centroids)
+    np.savez(f'../data/codebooks/emotion_resampled_to_cue/sikmeans_P-256_k-128_class-{i+1}_minutesPerIC-50_icsPerSubj-2_resampled.npz', centroids=new_centroids)
