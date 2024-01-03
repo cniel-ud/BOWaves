15075d49d738508df9d4f1b5705dbcf625f8d240
Austin <austin.meek10@gmail.com>
2024-01-03 12:22:00 -0500

    parallel slurm jobs for individual codebooks

--- hpc/bilal_data.qs
+++ hpc/bilal_data.qs
@@ -9,6 +9,7 @@
 #SBATCH --mail-type=END,FAIL,TIME_LIMIT_90
 #SBATCH --export=NONE
 #SBATCH --mem=64G
+#SBATCH --array=1-12
 #export UD_JOB_EXIT_FN_SIGNALS="SIGTERM EXIT"
 
 vpkg_devrequire intel-python/2022u1:python3
@@ -17,4 +18,4 @@ pip install seaborn
 
 # Run bash / python script below
 
-python ../tests/data_for_bilal.py
+python ../tests/data_for_bilal.py --codebook_subj=${SLURM_ARRAY_TASK_ID}
--- tests/data_for_bilal.py
+++ tests/data_for_bilal.py
@@ -7,9 +7,15 @@ The cue dataset's native sampling rate is 500 Hz, and I will resample to the sam
 This will also serve as an API guide for Bilal when he looks further into the code and writes his paper's method section.
 """
 import pyrootutils
+import argparse
 
 # un comment for caviness
-# pyrootutils.set_root(path='/work/cniel/ajmeek/BOWaves/BOWaves', pythonpath=True)
+pyrootutils.set_root(path='/work/cniel/ajmeek/BOWaves/BOWaves', pythonpath=True)
+
+parser = argparse.ArgumentParser(description="Your script description here")
+
+parser.add_argument("--codebook_subj", type=int, help="The subject to train individual neural codebook on.")
+args = parser.parse_args()
 
 from BOWaves.utilities.dataloaders import load_codebooks, load_raw_set, load_raw_set_single_subj
 from BOWaves.bowav.bowav_feature_extractor import bag_of_waves
@@ -25,134 +31,127 @@ from scipy.io import loadmat
 # changed to resample all cue subjects to the mice / emotion rates - 256 hz
 
 subj_list = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
-
-for subj in subj_list:
-    # load raw data
-    matdict = scipy.io.loadmat(f'../data/frolich/frolich_extract_{subj}.mat')
-
-    # get length of data
-    data_len = len(matdict['X'][1])
-    # resample to 256 hz
-    # matdict['X'] = scipy.signal.resample(matdict['X'], int((500.0/256) * data_len), axis=1) # should be 256/ 500.
-    # to double check, length of resampled divided by 256 should be equal to length of original divided by 500.
-
-    matdict['X'] = scipy.signal.resample(matdict['X'], int((256.0/500) * data_len), axis=1) # should be 256/ 500.
-
-    print(subj, data_len, len(matdict['X'][1]))
-
-    # save matdict to new file
-    scipy.io.savemat(f'../data/codebooks/frolich/frolich_extract_subj_{subj}_resampled_to_mice.mat', matdict)
-
-# # resample cue subj 01 to 256 hz
-# matdict = scipy.io.loadmat('../data/frolich/frolich_extract_01.mat')
 #
-# # signals stored in 'data' field
-# # original length is 1979155. So (500/256) * 1979155
-# matdict['X'] = scipy.signal.resample(matdict['X'], int((500.0/256) * 1979155), axis=1)
+# for subj in subj_list:
+#     # load raw data
+#     matdict = scipy.io.loadmat(f'../data/frolich/frolich_extract_{subj}.mat')
 #
-# # save matdict to new file
-# scipy.io.savemat('../data/codebooks/frolich/frolich_extract_subj_01_resampled_to_mice.mat', matdict)
+#     # get length of data
+#     data_len = len(matdict['X'][1])
+#     # resample to 256 hz
+#     # matdict['X'] = scipy.signal.resample(matdict['X'], int((500.0/256) * data_len), axis=1) # should be 256/ 500.
+#     # to double check, length of resampled divided by 256 should be equal to length of original divided by 500.
+#
+#     matdict['X'] = scipy.signal.resample(matdict['X'], int((256.0/500) * data_len), axis=1) # should be 256/ 500.
+#
+#     print(subj, data_len, len(matdict['X'][1]))
+#
+#     # save matdict to new file
+#     scipy.io.savemat(f'../data/codebooks/frolich/frolich_extract_subj_{subj}_resampled_to_mice.mat', matdict)
+
 
 
 # intermission - switch to HPC here to run codebooks. Then come back to desktop to run BOWaves
 # code run in between these lines was done on Caviness / desktop
 # ----------------------------------------------------------------------------------------------
-# file = '../data/cue/cue_signals_resampled_to_emotion/frolich_extract_subj_01_resampled_to_mice.mat'
-#
-#
-# # TODO - i resampled to a 256 sampling rate. The centroid len and window size need to change now to keep 1 sec centroid and 1.5 sec window.
-# # Redo this tomorrow.
-#
-# # these hyperparams are based on sampling rate. want 1 sec centroid, 1.5 sec window
-# window_len = 384
-# metric, init = 'cosine', 'random'
-# num_clusters = 128
-# centroid_len = 256
-# n_runs = 3
-# n_jobs = 1
-# rng = default_rng()
-#
-# # hold classified ICs. Technically here we only want the brain ones, but this is boilerplate at the moment
-# neural = {'name': 'neural', 'ICs': [], 'centroids': [], 'labels': [], 'shifts': [], 'distances': [], 'inertia': []}
-# blink = {'name': 'blink', 'ICs': [], 'centroids': [], 'labels': [], 'shifts': [], 'distances': [], 'inertia': []}
-# muscle = {'name': 'muscle', 'ICs': [], 'centroids': [], 'labels': [], 'shifts': [], 'distances': [], 'inertia': []}
-# mixed = {'name': 'mixed', 'ICs': [], 'centroids': [], 'labels': [], 'shifts': [], 'distances': [], 'inertia': []}
-# lateyes = {'name': 'lateyes', 'ICs': [], 'centroids': [], 'labels': [], 'shifts': [], 'distances': [], 'inertia': []}
-# heart = {'name': 'heart', 'ICs': [], 'centroids': [], 'labels': [], 'shifts': [], 'distances': [], 'inertia': []}
-#
-# all_classes = [neural, blink, muscle, mixed, lateyes, heart]
-#
-# ICs, labels = dataloaders.load_and_visualize_mat_file_frolich(file, visualize=False)
-#
-# # error check
-# if len(ICs) != len(labels):
-#     raise ValueError('ICs and labels are not the same length.')
-#
-# for i, label in enumerate(labels):
-#     if label == 'neural':
-#         neural['ICs'].append(ICs[i])
-#     elif label == 'blink':
-#         blink['ICs'].append(ICs[i])
-#     elif label == 'muscle':
-#         muscle['ICs'].append(ICs[i])
-#     elif label == 'mixed':
-#         mixed['ICs'].append(ICs[i])
-#     elif label == 'lateyes':
-#         lateyes['ICs'].append(ICs[i])
-#     elif label == 'heart':
-#         heart['ICs'].append(ICs[i])
-#     else:
-#         raise ValueError('Unknown class label: ' + label)
-#
-# tot_num_windows = 0  # to house total number of windows for all ICs
-# ic_lengths = []
-#
-# # now take the neural ICs and transform into right shape for sikmeans
-# for ic in neural['ICs']:
-#     ic_lengths.append(len(ic))
-#
-# # Currently, assuming that we are not taking a subset of the ICs at all. Carlos had the option for that in his earlier window code.
-# # So the number of windows per ic will just be the length of each ic / win len.
-# n_windows_per_ic = [ic_len // window_len for ic_len in ic_lengths]
-# tot_num_windows += sum(n_windows_per_ic)
-#
-#
-# # Now that we have the number of windows per ic, we can create the windows.
-# #rng = np.random.RandomState(42)
-#
-# X = np.zeros((tot_num_windows, window_len))  # X is for each class. Stack later
-# win_start = 0
-# # for label in all_classes:
-# for ic in neural['ICs']:
-#     windows_per_ic = len(ic) // window_len
-#     time_idx = np.arange(0, len(ic) - window_len + 1, window_len)
-#     time_idx = rng.choice(time_idx, size=windows_per_ic, replace=False)
-#     time_idx = time_idx[:, None] + np.arange(window_len)[None, :]
-#
-#
-#     # There seems to be an off by one error here.
-#     # The very last IC goes past the total num of windows in X.
-#     # Not sure if it's off by one, since the first portion of X is filled. Perhaps it's about how I calc
-#     # the total num of ICs? - don't think so. So cut off at the last time.
-#
-#     if (win_start == tot_num_windows):
-#         break
-#
-#     X[win_start:win_start + windows_per_ic] = ic[time_idx]
-#     win_start += windows_per_ic
-#
-# neural_windows = X
-#
-#
-# # train codebook for neural data
-# neural['centroids'], neural['labels'], neural['shifts'], neural['distances'], neural['inertia'], _ = \
-#     shift_invariant_k_means(neural_windows, num_clusters, centroid_len, metric=metric, init=init, n_init=n_runs)#, n_jobs)#, rng)
-#
-# # save codebook
-# np.savez(f'../data/codebooks/frolich/sikmeans_P-{window_len}_k-{num_clusters}_class-neural_subj-1_resampled.npz',
-#          centroids=neural['centroids'], labels=neural['labels'], shifts=neural['shifts'], distances=neural['distances'],
-#          inertia=neural['inertia'])
-#
+# some slight formatting issues with slurm arrays:
+if int(args.codebook_subj) < 10:
+    args.codebook_subj = '0' + str(args.codebook_subj)
+else:
+    args.codebook_subj = str(args.codebook_subj)
+file = f'../data/cue/cue_signals_resampled_to_emotion/frolich_extract_subj_{args.codebook_subj}_resampled_to_mice.mat'
+
+
+# these hyperparams are based on sampling rate. want 1 sec centroid, 1.5 sec window
+window_len = 384
+metric, init = 'cosine', 'random'
+num_clusters = 128
+centroid_len = 256
+n_runs = 3
+n_jobs = 1
+rng = default_rng()
+
+# hold classified ICs. Technically here we only want the brain ones, but this is boilerplate at the moment
+neural = {'name': 'neural', 'ICs': [], 'centroids': [], 'labels': [], 'shifts': [], 'distances': [], 'inertia': []}
+blink = {'name': 'blink', 'ICs': [], 'centroids': [], 'labels': [], 'shifts': [], 'distances': [], 'inertia': []}
+muscle = {'name': 'muscle', 'ICs': [], 'centroids': [], 'labels': [], 'shifts': [], 'distances': [], 'inertia': []}
+mixed = {'name': 'mixed', 'ICs': [], 'centroids': [], 'labels': [], 'shifts': [], 'distances': [], 'inertia': []}
+lateyes = {'name': 'lateyes', 'ICs': [], 'centroids': [], 'labels': [], 'shifts': [], 'distances': [], 'inertia': []}
+heart = {'name': 'heart', 'ICs': [], 'centroids': [], 'labels': [], 'shifts': [], 'distances': [], 'inertia': []}
+
+all_classes = [neural, blink, muscle, mixed, lateyes, heart]
+
+ICs, labels = dataloaders.load_and_visualize_mat_file_frolich(file, visualize=False)
+
+# error check
+if len(ICs) != len(labels):
+    raise ValueError('ICs and labels are not the same length.')
+
+for i, label in enumerate(labels):
+    if label == 'neural':
+        neural['ICs'].append(ICs[i])
+    elif label == 'blink':
+        blink['ICs'].append(ICs[i])
+    elif label == 'muscle':
+        muscle['ICs'].append(ICs[i])
+    elif label == 'mixed':
+        mixed['ICs'].append(ICs[i])
+    elif label == 'lateyes':
+        lateyes['ICs'].append(ICs[i])
+    elif label == 'heart':
+        heart['ICs'].append(ICs[i])
+    else:
+        raise ValueError('Unknown class label: ' + label)
+
+tot_num_windows = 0  # to house total number of windows for all ICs
+ic_lengths = []
+
+# now take the neural ICs and transform into right shape for sikmeans
+for ic in neural['ICs']:
+    ic_lengths.append(len(ic))
+
+# Currently, assuming that we are not taking a subset of the ICs at all. Carlos had the option for that in his earlier window code.
+# So the number of windows per ic will just be the length of each ic / win len.
+n_windows_per_ic = [ic_len // window_len for ic_len in ic_lengths]
+tot_num_windows += sum(n_windows_per_ic)
+
+
+# Now that we have the number of windows per ic, we can create the windows.
+#rng = np.random.RandomState(42)
+
+X = np.zeros((tot_num_windows, window_len))  # X is for each class. Stack later
+win_start = 0
+# for label in all_classes:
+for ic in neural['ICs']:
+    windows_per_ic = len(ic) // window_len
+    time_idx = np.arange(0, len(ic) - window_len + 1, window_len)
+    time_idx = rng.choice(time_idx, size=windows_per_ic, replace=False)
+    time_idx = time_idx[:, None] + np.arange(window_len)[None, :]
+
+
+    # There seems to be an off by one error here.
+    # The very last IC goes past the total num of windows in X.
+    # Not sure if it's off by one, since the first portion of X is filled. Perhaps it's about how I calc
+    # the total num of ICs? - don't think so. So cut off at the last time.
+
+    if (win_start == tot_num_windows):
+        break
+
+    X[win_start:win_start + windows_per_ic] = ic[time_idx]
+    win_start += windows_per_ic
+
+neural_windows = X
+
+
+# train codebook for neural data
+neural['centroids'], neural['labels'], neural['shifts'], neural['distances'], neural['inertia'], _ = \
+    shift_invariant_k_means(neural_windows, num_clusters, centroid_len, metric=metric, init=init, n_init=n_runs)#, n_jobs)#, rng)
+
+# save codebook
+np.savez(f'../data/codebooks/frolich/sikmeans_P-{window_len}_k-{num_clusters}_class-neural_subj-{args.codebook_subj}_resampled.npz',
+         centroids=neural['centroids'], labels=neural['labels'], shifts=neural['shifts'], distances=neural['distances'],
+         inertia=neural['inertia'])
+
 
 # ----------------------------------------------------------------------------------------------
 
