b72889103dbbe43dd09e7c5ecf77efe6986863bb
Austin <austin.meek10@gmail.com>
2024-01-03 16:15:18 -0500

    test clf on resampled cue data at 256 hz

--- tests/test_clf.py
+++ tests/test_clf.py
@@ -122,7 +122,7 @@ for subj_id in subj_ids:
     # changed above. we want cue and emotion to be on same sampling rate, so use the resampled frolich data
     raw_ics, y, labels = load_raw_set_single_subj_drb_frolich_extract(args, rng,
                                                                       data_dir=Path('../data/cue/cue_signals_resampled_to_emotion'),
-                                                                      fnames=[f'frolich_extract_{subj_id}_resampled_to_mice.mat'])
+                                                                      fnames=[f'frolich_extract_subj_{subj_id}_resampled_to_mice.mat'])
 
     codebook_args = copy.deepcopy(args)
     codebook_args.minutes_per_ic = args.codebook_minutes_per_ic
