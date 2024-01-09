8d58769bd93860889c67481e8ddb9df2ffaf5714
Austin <austin.meek10@gmail.com>
2024-01-08 20:09:04 -0500

    psd viz

--- tests/psd_visualization.py
+++ tests/psd_visualization.py
@@ -6,17 +6,48 @@ Want to visualize different data preprocessing of emotion & cue EEG datasets.
 On plane, visualize frolich sampled and resampled. Wait until better internet connection to download and test emotion.
 """
 
-title = 'frolich_extract_subj_03_resampled_to_mice_hp_filtered.mat'
+# title = 'frolich_extract_subj_03_resampled_to_mice_hp_filtered.mat'
+#
+# frolich_resampled = loadmat(f'../data/codebooks/frolich/{title}')
+#
+# pxx, freqs = plt.psd(frolich_resampled['X'], Fs=256)
 
-frolich_resampled = loadmat(f'../data/codebooks/frolich/{title}')
 
-pxx, freqs = plt.psd(frolich_resampled['X'], Fs=256)
+
+# Create subplots in a 3x4 grid
+fig, axs = plt.subplots(3, 4, figsize=(15, 10))
+
+# Flatten the 3x4 subplot array to make it easier to index
+axs = axs.flatten()
+
+
+num_signals = 12
+# Plot each signal
+for i in range(num_signals):
+    # Load data
+    if i < 9:
+        title = f'frolich_extract_subj_0{i+1}_resampled_to_mice_lp_filtered.mat'
+    else:
+        title = f'frolich_extract_subj_{i+1}_resampled_to_mice_lp_filtered.mat'
+    #title = f'frolich_extract_subj_{i+1}_resampled_to_mice_hp_filtered.mat'
+    frolich_resampled = loadmat(f'../data/codebooks/frolich/{title}')
+
+    #pxx, freqs = plt.psd(frolich_resampled['X'], Fs=256)
+    pxx, freqs = axs[i].psd(frolich_resampled['X'], Fs=256)
+    axs[i].set_title(f'Subject {i + 1}')
+
+# Add a title to the entire figure
+fig.suptitle('PSD of Frolich Resampled EEG Data Low Pass Filtered (should be noise)', fontsize=16)
+
+# Adjust layout to prevent overlapping
+plt.tight_layout(rect=[0, 0, 1, 0.96])
 
 #fig, axs = plt.subplots()
 
-plt.title(title)
+#plt.title(title)
 
-plt.show()
+#plt.show()
+plt.savefig('../data/codebooks/frolich/frolich_resampled_psd_lp_filtered.png')
 
 # arrange these later to be in a 3x4 grid etc
 
